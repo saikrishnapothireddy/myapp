73ca999a6b1716ba79a6a1594ef67385c458d5f7
saikrishna <saikrishnapothireddy@gmail.com>
2022-02-11 00:19:39 +0530

    a

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'demoapp2481.herokuapp.com'
+  host = "demoapp2481.herokuapp.com"
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
