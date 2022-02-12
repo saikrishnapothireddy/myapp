60e418acb5d737f9313cb02b1289f8119f70d460
saikrishna <saikrishnapothireddy@gmail.com>
2022-02-12 06:11:03 +0530

    added static assets true in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.serve_static_assets = true
   config.assets.compile = true
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   config.assets.digest = true
 
   # // delete precompiled assets
